ef2403eb79f6aac02b656a337726395a4027c1c5
George <84743235+nikibeep@users.noreply.github.com>
2023-02-10 09:44:13 +0530

    modified:   javaPrgm/.vscode/settings.json 	deleted:    javaPrgm/bin/Customer.class 	new file:   javaPrgm/bin/DataTypeTesting.class 	modified:   javaPrgm/bin/Main.class 	new file:   javaPrgm/bin/Main1.class 	new file:   javaPrgm/bin/Main2.class 	modified:   javaPrgm/bin/Student.class 	modified:   javaPrgm/bin/Worker.class 	new file:   javaPrgm/bin/Workers.class 	deleted:    javaPrgm/src/DataTypeTesting$Main.class 	deleted:    javaPrgm/src/DataTypeTesting.class 	modified:   javaPrgm/src/DataTypeTesting.java 	new file:   javaPrgm/src/Main1.java 	new file:   javaPrgm/src/Main2.java     modified:   javaPrgm/src/Student.java

--- javaPrgm/src/DataTypeTesting.java
+++ javaPrgm/src/DataTypeTesting.java
@@ -1,65 +1,167 @@
 import java.io.*;
 import java.util.Scanner;
 
-class DataTypeTesting {
-  String name;
-  byte age;
-  Long accNo;
-  char gender;
-  short minBal;
-  float bal;
-  long custID;
-  boolean pan;
+class Customer
+{
+	//data member
+	String name;
+	byte age;
+	long acc;
+	char gender;
+	short minbal;
+	float bal;
+	boolean pan;
 
-  public void read() throws IOException {
-    InputStreamReader isr = new InputStreamReader(System.in);
-    BufferedReader br = new BufferedReader(isr);
-    try (Scanner scan = new Scanner(System.in)) {
-		System.out.println("Enter the customer name:");
-		name = br.readLine();
-
-		System.out.println("Enter the customer age:");
-		age = Byte.parseByte(br.readLine());
-
-		System.out.println("Enter the customer bank account number:");
-		accNo = Long.parseLong(br.readLine());
-
-		System.out.println("Enter the gender:");
-		gender = scan.next().charAt(0);
-	} catch (NumberFormatException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	//default-constructor
+	public Customer()
+	{
+		name="";
+		age=0;
+		acc=0;
+		gender='c';
+		minbal=0;
+		bal=0;
+		pan=false;
 	}
 
-    System.out.println("Enter the Minimum balance:");
-    minBal = Short.parseShort(br.readLine());
+	public Customer(String name)
+	{
+		this.name = name;// this keyword is used to refer to data member of the current object,whenever data member and                          						    //parameter are same
+	}
 
-    System.out.println("Enter the Balance amount:");
-    bal = Float.parseFloat(br.readLine());
+	public Customer(String name, long acc)
+	{
+		this(name);//constructor chaining
+		this.acc = acc;
+	}
 
-    System.out.println("Enter the customer ID:");
-    custID = Long.parseLong(br.readLine());
+	public boolean search(String csname)
+	{
+		boolean f = false;
+		if(name.equals(csname))
+		{
+			f = true;
+		}
+		return f;
+	}
 
-    System.out.println("Enter PAN:");
-    pan = Boolean.parseBoolean(br.readLine());
-  }
+	public boolean search(int acc_no)
+	{
+		boolean f = false;
+		if(acc==acc_no)
+		{
+			f = true;
+		}
+		return f;
+	}
 
-  public void display() {
-    System.out.println("Customer name = " + name);
-    System.out.println("Customer age = " + age);
-    System.out.println("Customer Account number = " + accNo);
-    System.out.println("Customer gender = " + gender);
-    System.out.println("Customer Minimum balance = " + minBal);
-    System.out.println("Customer Account Balance = " + bal);
-    System.out.println("Customer Customer ID = " + custID);
-    System.out.println("Customer PAN = " + pan);
-  }
+	//member function
+	public void read() throws IOException
+	{
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the customer name:");
+		name = br.readLine();
+		System.out.println("Enter the customer age:");
+		age = Byte.parseByte(br.readLine());
+		System.out.println("Enter the account number:");
+		acc = Long.parseLong(br.readLine());
+		System.out.println("Enter the minimum balance:");
+		minbal = Short.parseShort(br.readLine());
+		System.out.println("Enter the balance amount:");
+		bal = Float.parseFloat(br.readLine());
+		System.out.println("Enter the customer gender:");
+		gender = (char)br.read();
+		String ch = br.readLine();
+		System.out.println("Do you have Pan Card:");
+		pan = Boolean.parseBoolean(br.readLine());
+	}
+	
+	public void disp()
+	{
+		System.out.println("Customer Name:"+name);
+		System.out.println("Customer Age:"+age);
+		System.out.println("Age:"+age);
+		System.out.println("Account Number:"+acc);
+		System.out.println("Gender:"+gender);
+		System.out.println("PAN:"+pan);
+	}
 }
+public class DataTypeTesting
+{
+	public static void main(String args[]) throws IOException
+	{
+		/*Customer ob1 = new Customer();//default constructor
+		Customer ob2 = new Customer("Harsh Vardhan Singh");
+		Customer ob3 = new Customer("Aryan Raj",123456789);
+		//ob1.read();
+		ob1.disp();
+		ob2.disp();
+		ob3.disp();*/
+	
 
-public class Main {
-  public static void main(String[] args) throws IOException {
-    DataTypeTesting dtt = new DataTypeTesting();
-    dtt.read();
-    dtt.display();
-  }
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		boolean ch=true;
+		byte res,res1,b1,temp=0;
+		String sname;
+		Customer obj[] = new Customer[3];
+		while(ch)
+		{
+			System.out.println("1.read data");
+			System.out.println("2.display data");
+			System.out.println("3.Search");
+			System.out.println("4.Exit");
+			System.out.println("Enter your choice:");
+			res = Byte.parseByte(br.readLine());
+			switch(res)
+			{
+				case 1:
+					for(byte i=0;i<3;i++)
+					{
+						obj[i] = new Customer();
+						obj[i].read();
+					}
+					break;
+				case 2:
+					for(byte i=0;i<3;i++)
+					{
+						obj[i].disp();
+					}
+					break;
+				case 3:
+					System.out.println("1.Search by Name");
+					System.out.println("2.Search by account no.");
+					res1 = Byte.parseByte(br.readLine());
+					switch(res1)
+					{
+						case 1:
+							System.out.println("Enter the name to be searched:");
+							sname = br.readLine();
+							for(byte i=0;i<3;i++)
+							{
+								if(obj[i].search(sname)==true)
+								{
+									temp = 1;
+									obj[i].disp();
+								}
+							}
+								
+							if(temp==0)
+							{
+								System.out.println("Record doesn't exist");
+							}
+							break;
+				
+						case 2:
+							break;
+					}
+				case 4:
+					ch = false;
+					break;
+			}
+		}
+	}
 }
---
+++ javaPrgm/src/Main2.java
@@ -0,0 +1,124 @@
+class DataTypeTesting {
+  String name;
+  byte age;
+  Long accNo;
+  char gender;
+  short minBal;
+  float bal;
+  long custID;
+  boolean pan;
+
+  public DataTypeTesting() {
+    // default constructor
+  }
+  
+  public DataTypeTesting(String name, byte age, Long accNo, char gender, short minBal, float bal, long custID, boolean pan) {
+    this.name = name;
+    this.age = age;
+    this.accNo = accNo;
+    this.gender = gender;
+    this.minBal = minBal;
+    this.bal = bal;
+    this.custID = custID;
+    this.pan = pan;
+  }
+  
+  public void read() throws IOException {
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(isr);
+    Scanner scan = new Scanner(System.in);
+    try {
+      System.out.println("Enter the customer name:");
+      name = br.readLine();
+
+      System.out.println("Enter the customer age:");
+      age = Byte.parseByte(br.readLine());
+
+      System.out.println("Enter the customer bank account number:");
+      accNo = Long.parseLong(br.readLine());
+
+      System.out.println("Enter the gender:");
+      gender = scan.next().charAt(0);
+    } catch (NumberFormatException e) {
+      e.printStackTrace();
+    } finally {
+      scan.close();
+    }
+
+    System.out.println("Enter the Minimum balance:");
+    minBal = Short.parseShort(br.readLine());
+
+    System.out.println("Enter the Balance amount:");
+    bal = Float.parseFloat(br.readLine());
+
+    System.out.println("Enter the customer ID:");
+    custID = Long.parseLong(br.readLine());
+
+    System.out.println("Enter PAN:");
+    pan = Boolean.parseBoolean(br.readLine());
+  }
+
+  public void read(String name, byte age, Long accNo, char gender, short minBal, float bal, long custID, boolean pan) {
+    this.name = name;
+    this.age = age;
+    this.accNo = accNo;
+    this.gender = gender;
+    this.minBal = minBal;
+    this.bal = bal;
+    this.custID = custID;
+    this.pan = pan;
+  }
+
+  public void display() {
+    System.out.println("Customer name = " + name);
+    System.out.println("Customer age = " + age);
+    System.out.println("Customer Account number = " + accNo);
+    System.out.println("Customer gender = " + gender);
+    System.out.println("Customer Minimum balance = " + minBal);
+    System.out.println("Customer Account Balance = " + bal);
+    System.out.println("Customer
+
+
+  public void display() {
+    System.out.println("Customer name = " + name);
+    System.out.println("Customer age = " + age);
+    System.out.println("Customer Account number = " + accNo);
+    System.out.println("Customer gender = " + gender);
+    System.out.println("Customer Minimum balance = " + minBal);
+    System.out.println("Customer Account Balance = " + bal);
+    System.out.println("Customer Customer ID = " + custID);
+    System.out.println("Customer PAN = " + pan);
+  }
+}
+
+public class Main2 {
+  static DataTypeTesting dtt = new DataTypeTesting();
+
+  public static void main(String[] args) throws IOException {
+    int choice;
+    try (Scanner scan = new Scanner(System.in)) {
+        do {
+          System.out.println("\nMenu:");
+          System.out.println("1. Read data");
+          System.out.println("2. Display data");
+          System.out.println("3. Exit");
+          System.out.println("Enter your choice:");
+          choice = scan.nextInt();
+
+          switch(choice) {
+            case 1:
+              dtt.read();
+              break;
+            case 2:
+              dtt.display();
+              break;
+            case 3:
+              System.exit(0);
+              break;
+            default:
+              System.out.println("Invalid choice. Please try again.");
+          }
+        } while(choice != 3);
+    }
+  }
+}
